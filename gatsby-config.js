47d5ab6477fd90f207d893dafe8548b263d28402
TTYL <harpreetsinghsn@gmail,com>
2021-02-22 13:26:32 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: "my-blog",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "",
+        spaceId: "",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
